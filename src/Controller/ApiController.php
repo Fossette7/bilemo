8e2733fe0ee73f092a0afcc76b946d0484955093
Reybeka Peyrat <reybeka.ronel@gmail.com>
2022-09-08 14:18:32 +0200

    add create and show mobile function

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,12 +8,13 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api', methods: ['GET'])]
+    #[Route('/api/mobile', name: 'app_api', methods: ['GET'])]
     public function index(MobileRepository $mobileRepository, SerializerInterface $serializer ): JsonResponse
     {
       $mobiles = $mobileRepository->findAll();
@@ -21,10 +22,47 @@ class ApiController extends AbstractController
       $jsonContent = $serializer->serialize($mobiles, 'json');
 
       $response = new JsonResponse($jsonContent, 200, [], true);
-      return $this->json([
-          'obj' => $jsonContent,
-          'path' => 'src/Controller/ApiController.php',
-      ]);
-      return $jsonContent;
+
+      return $response;
+    }
+
+  #[Route('/api/mobile/{id}', name: 'app_api_mobile_show', methods: ['GET'])]
+  public function mobileShow(Mobile $mobile, SerializerInterface $serializer ): JsonResponse
+  {
+    $jsonContent = $serializer->serialize($mobile, 'json');
+
+    $response = new JsonResponse($jsonContent, 200, [], true);
+    return $response;
+  }
+
+    #[Route('/api/mobile', name: 'app_api_create', methods: ['POST'])]
+    public function create(Request $request, MobileRepository $mobileRepository, EntityManagerInterface $em, SerializerInterface $serializer)
+    {
+      $jsonData = json_decode($request->getContent(),1);
+      if(!empty($jsonData['brandName'])){
+        $brandName = $jsonData['brandName'];
+
+        // Check if brand already exist
+        $mobileObject = $mobileRepository->findBy(['brandname' => $brandName]);
+
+        if(!empty($mobileObject)){
+          return new JsonResponse(['error' => sprintf('This %s already exist', $brandName)]);
+        }
+
+        $newMobile = new Mobile();
+        $newMobile->setBrandname($brandName);
+        $em->persist($newMobile);
+        $em->flush();
+        $jsonMobile = $serializer->serialize([
+          'success' => sprintf('%s Add', $brandName),
+          'object' => $newMobile
+        ], 'json');
+
+        $response = new JsonResponse($jsonMobile, 200, [], true);
+
+        return $response;
+      }
+
+      return new JsonResponse(['error' => 'brandName data missing']);
     }
 }
