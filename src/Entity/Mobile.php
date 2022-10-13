50ce2db5601faf7a10874ffe90642eb4cb37649d
Reybeka Peyrat <reybeka.ronel@gmail.com>
2022-10-13 16:02:44 +0200

    add new Mobile properties

--- src/Entity/Mobile.php
+++ src/Entity/Mobile.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MobileRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -18,6 +19,29 @@ class Mobile
     #[Assert\NotBlank]
     private ?string $brandname = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Model = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $stock = null;
+
+    public function __contruct()
+    {
+      $this->createdAt = new \DateTime();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -31,6 +55,78 @@ class Mobile
     public function setBrandname(string $brandname): self
     {
         $this->brandname = $brandname;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->Model;
+    }
+
+    public function setModel(?string $Model): self
+    {
+        $this->Model = $Model;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): self
+    {
+        $this->stock = $stock;
 
         return $this;
     }
