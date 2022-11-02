2aa714c96cfd23a7f0002dcafd0c2bb38ca96e00
Reybeka Peyrat <reybeka.ronel@gmail.com>
2022-11-02 14:18:34 +0100

    add cache

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,20 +15,26 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Nelmio\ApiDocBundle\Annotation\Security;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ApiController extends AbstractController
 {
   #[Route('/api/mobiles', name: 'app_api', methods: ['GET'])]
 
-  public function getMobileListPagination(MobileRepository $mobileRepository, SerializerInterface $serializer, Request $request ): JsonResponse
+  public function getMobileListPagination(MobileRepository $mobileRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cachePool ): JsonResponse
   {
     if(!empty($page = $request->get('page', 1)) && !empty($limit = $request->get('limit', 3))){
 
-    $mobiles = $mobileRepository->findAllPagination($page, $limit);
+      $idCache = "getAllMobiles-" . $page . "-" . $limit;
+      $mobileList = $cachePool->get($idCache, function (ItemInterface $item) use ($mobileRepository, $page, $limit){
+        $item->tag("mobilesCache");
+        return $mobileRepository->findAllPagination($page, $limit);
+      });
 
-    $jsonContent = $serializer->serialize($mobiles, 'json');
+      $jsonContent = $serializer->serialize($mobileList, 'json');
 
-    $response = new JsonResponse($jsonContent, 200, [], true);
+      $response = new JsonResponse($jsonContent, 200, [], true);
     }
 
     return $response;
@@ -37,8 +43,9 @@ class ApiController extends AbstractController
 
   #[Route('/api/mobile/{id}', name: 'api_show_mobile', methods: ['GET'])]
   #[Security(name: 'Bearer')]
-  public function showMobile(Mobile $mobile, SerializerInterface $serializer ): JsonResponse
+  public function showMobile(Mobile $mobile, SerializerInterface $serializer): JsonResponse
   {
+
     $jsonContent = $serializer->serialize($mobile, 'json');
 
     $response = new JsonResponse($jsonContent, 200, [], true);
@@ -50,6 +57,7 @@ class ApiController extends AbstractController
     public function createMobile(Request $request, SerializerInterface $serializer, EntityManagerInterface $em) :JsonResponse
     {
       $mobile = $serializer->deserialize($request->getContent(), Mobile::class, 'json');
+
       $em->persist($mobile);
       $em->flush();
 
@@ -78,9 +86,9 @@ class ApiController extends AbstractController
 
     #[Route('/api/mobile/{id}', name: 'api_delete_mobile', methods: ['DELETE'])]
     #[Security(name: 'Bearer')]
-    public function deleteMobile(Mobile $mobile, EntityManagerInterface $em): JsonResponse
+    public function deleteMobile(Mobile $mobile, EntityManagerInterface $em, TagAwareCacheInterface $cachePool): JsonResponse
     {
-
+      $cachePool->invalidateTags(["mobilesCache"]);
       $em->remove($mobile);
       $em->flush();
       return new JsonResponse(null, Response::HTTP_NO_CONTENT);
