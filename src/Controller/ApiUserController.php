4e52b92b240876be599a25e1350019702fb6585b
Reybeka Peyrat <reybeka.ronel@gmail.com>
2022-11-19 10:50:50 +0100

    Replace attributes to annotion

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Customer;
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\UserRepository;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,12 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Entity\Customer;
+use App\Entity\User;
+use App\Repository\UserRepository;
 
 
 class ApiUserController extends AbstractController
 {
-  #[Route('/api/users', name: 'api_userslist', methods: ['GET'])]
-  #[Security(name: 'Bearer')]
+  /**
+   * @Route("/api/users", name="api_userslist", methods={"GET"})
+   * @Security(name="Bearer")
+   */
   public function getUserList(
     UserRepository $userRepository,
     Request $request,
@@ -28,7 +31,6 @@ class ApiUserController extends AbstractController
   ): JsonResponse {
     // Récupération de l'utilisateur connecté à l'api via le token associé au service TokenStorageInterface (JWT)
     $customer = $token->getToken()->getUser();
-    dump($customer);die;
     if (!empty($page = $request->get('page', 1)) && !empty($limit = $request->get('limit', 3))) {
       // On envoie l'objet Customer dans la méthode findAllPagination
       $users = $userRepository->findAllPagination($customer, $page, $limit);
@@ -40,8 +42,10 @@ class ApiUserController extends AbstractController
 
   }
 
-  #[Route('/api/user/{id}', name: 'api_show_user', methods: ['GET'])]
-  #[Security(name: 'Bearer')]
+  /**
+   * @Route("/api/user/{id}", name="api_show_user", methods={"GET"})
+   * @Security(name="Bearer")
+   */
   public function getUserDetail(User $user, SerializerInterface $serializer): JsonResponse
   {
     // Récupération du token pour avoir le customer
@@ -56,9 +60,11 @@ class ApiUserController extends AbstractController
 
   }
 
-  #[Route('/api/user', name: 'api_create_user', methods: ['POST'])]
-  #[IsGranted('ROLE_ADMIN')]
-  #[Security(name: 'Bearer')]
+  /**
+   * @Route("/api/user", name="api_create_user", methods={"POST"})
+   * @Security(name="Bearer")
+   * @IsGranted("ROLE_ADMIN")
+   */
   public function createUser(
     Request $request,
     SerializerInterface $serializer,
@@ -80,9 +86,11 @@ class ApiUserController extends AbstractController
 
   }
 
-  #[Route('/api/user/{id}', name: 'api_delete_user', methods: ['DELETE'])]
-  #[IsGranted('ROLE_ADMIN')]
-  #[Security(name: 'Bearer')]
+  /**
+   * @Route("/api/user", name="api_create_user", methods={"DELETE"})
+   * @Security(name="Bearer")
+   * @IsGranted("ROLE_ADMIN")
+   */
   public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse
   {
 
